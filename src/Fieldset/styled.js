826a3cc73962a8273aad184a8dbbbde97db767d9
Kornel Masnica <kornel.masnica@gmail.com>
2024-02-05 23:43:45 +0100

    added box shadows

--- src/Fieldset/styled.js
+++ src/Fieldset/styled.js
@@ -6,6 +6,9 @@ export const StyledFieldset = styled.fieldset`
     border: 1px solid #323232;
     border-radius: 5px;
     margin: 10px 0 60px;
+    -webkit-box-shadow: 8px 8px 24px 0px rgba(66, 68, 90, 1);
+    -moz-box-shadow: 8px 8px 24px 0px rgba(66, 68, 90, 1);
+    box-shadow: 8px 8px 24px 0px rgba(66, 68, 90, 1);
 
     @media (max-width: 530px) {
         padding: 10px 5px;
