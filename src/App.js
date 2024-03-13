d254f0da43b4c5f87e9d868b117f71f2b1d4f2c1
Kornel Masnica <kornel.masnica@gmail.com>
2024-03-13 20:46:30 +0100

    created custom hook for loading currencies and update date

--- src/App.js
+++ src/App.js
@@ -6,45 +6,24 @@ import Container from "./Container"
 import Fieldset from "./Fieldset";
 import Article from "./Article";
 import Clock from "./Clock";
-import { useState, useEffect } from "react";
-import axios from "axios";
+import useCurrencies from "./Currencies/useCurrencies";
+import { useState} from "react";
 
 function App() {
 
-  useEffect(() => {
-    const loadCurrencies = async () => {
-      try {
-        const response = await axios.get("/ratesx.json");
-        const plnValue = response.data.data.PLN.value;
-        const includeCurrencies = ["EUR", "USD", "CHF", "GBP"];
-        const currencyNames = {
-          EUR: "Euro",
-          USD: "Dolar amerykański",
-          CHF: "Frank szwajcarski",
-          GBP: "Funt brytyjski"
-        }
-        const filteredCurrencies = Object.values(response.data.data).filter(currency => includeCurrencies.includes(currency.code));
-        const loadedCurrencies = filteredCurrencies.map(currency => ({
-          ...currency,
-          value: plnValue / currency.value,
-          name: currencyNames[currency.code]
-        }));
-        setCurrencies(loadedCurrencies);
-        setSelectedForeign(loadedCurrencies[0]);
-        setSelectedNational(loadedCurrencies[0]);
-      } catch (error) {
-        console.error("Coś się popsuło", error);
-      }
-    };
+  const {
+    lastUpdate,
+    setLastUpdate,
+    currencies,
+    setCurrencies,
+    selectedForeign,
+    setSelectedForeign,
+    selectedNational,
+    setSelectedNational
+  } = useCurrencies();
 
-    loadCurrencies();
-  }, []);
-
-  const [currencies, setCurrencies] = useState([]);
   const [selectedCurrency, setSelectedCurrency] = useState({ code: "", name: "", buy: "N/A", sell: "N/A" });
-  const [selectedForeign, setSelectedForeign] = useState({ code: "", name: "", buy: "N/A", sell: "N/A" });
-  const [selectedNational, setSelectedNational] = useState({ code: "", name: "", buy: "N/A", sell: "N/A" });
-  
+
   const [amountToPln, setAmountToPln] = useState("");
   const [amountFromPln, setAmountFromPln] = useState("");
 
@@ -85,129 +64,130 @@ function App() {
     <Article
       form={
         currencies.length > 0 ? (
-        <>
-          <Clock />
-          <Fieldset
-            title="Kursy walut"
-            body={
-              <>
-                <Container
-                  textAlign="center"
-                  body={
-                    <>
-                      <Label
-                        forContent="rates"
-                        labelText="Dla:"
-                      />
-                      <Select
-                        name="rates"
-                        id="rates"
-                        defaultValue=""
-                        extraOption="Wybierz walutę"
-                        onChange={handleCurrencySelect}
-                        currencies={currencies}
-                        renderOptionContent={(currency) => `${currency.code} (${currency.name})`}
-                      />
-                    </>
-                  }
-                />
-                <p>
-                  <Label
-                    labelText="Kupno:"
+          <>
+            <Clock />
+            <Fieldset
+              title="Kursy walut"
+              body={
+                <>
+                  <Container
+                    textAlign="center"
+                    body={
+                      <>
+                        <Label
+                          forContent="rates"
+                          labelText="Dla:"
+                        />
+                        <Select
+                          name="rates"
+                          id="rates"
+                          defaultValue=""
+                          extraOption="Wybierz walutę"
+                          onChange={handleCurrencySelect}
+                          currencies={currencies}
+                          renderOptionContent={(currency) => `${currency.code} (${currency.name})`}
+                        />
+                      </>
+                    }
                   />
-                  <strong>{typeof selectedCurrency.value === "number" ? (selectedCurrency.value * 0.99).toFixed(2) : "N/A"} </strong>
-                  PLN
-                </p>
-                <p>
-                  <Label
-                    labelText="Sprzedaż:"
-                  />
-                  <strong>{typeof selectedCurrency.value === "number" ? (selectedCurrency.value * 1.01).toFixed(2) : "N/A"} </strong>
-                  PLN
-                </p>
-              </>
-            }
-          />
+                  <p>
+                    <Label
+                      labelText="Kupno:"
+                    />
+                    <strong>{typeof selectedCurrency.value === "number" ? (selectedCurrency.value * 0.99).toFixed(2) : "N/A"} </strong>
+                    PLN
+                  </p>
+                  <p>
+                    <Label
+                      labelText="Sprzedaż:"
+                    />
+                    <strong>{typeof selectedCurrency.value === "number" ? (selectedCurrency.value * 1.01).toFixed(2) : "N/A"} </strong>
+                    PLN
+                  </p>
+                  <p center>Data ostatniej aktualizacji: <b>{lastUpdate.toLocaleDateString()}</b></p>
+                </>
+              }
+            />
 
-          <Fieldset
-            title="Wymiana na złotówki"
-            body={
-              <>
-                <Container
-                  body={
-                    <>
-                      <Label
-                        forContent="amountToPln"
-                        labelText="Posiadam:"
-                      />
-                      <Input
-                        value={amountToPln}
-                        onChange={handleToPlnInput}
-                        id="amountToPln"
-                        onKeyDown={handleKeyDown}
-                      />
-                      <Select
-                        name="toPln"
-                        id="toPln"
-                        defaultValue={currencies[0].code}
-                        onChange={handleToPlnSelect}
-                        currencies={currencies}
-                        renderOptionContent={(currency) => currency.code}
-                      />
-                    </>
-                  }
-                />
-                <p>
-                  <Label
-                    labelText="Otrzymam:"
+            <Fieldset
+              title="Wymiana na złotówki"
+              body={
+                <>
+                  <Container
+                    body={
+                      <>
+                        <Label
+                          forContent="amountToPln"
+                          labelText="Posiadam:"
+                        />
+                        <Input
+                          value={amountToPln}
+                          onChange={handleToPlnInput}
+                          id="amountToPln"
+                          onKeyDown={handleKeyDown}
+                        />
+                        <Select
+                          name="toPln"
+                          id="toPln"
+                          defaultValue={currencies[0].code}
+                          onChange={handleToPlnSelect}
+                          currencies={currencies}
+                          renderOptionContent={(currency) => currency.code}
+                        />
+                      </>
+                    }
                   />
-                  <strong> {calculateToPlnResult()} </strong>
-                  PLN
-                </p>
-              </>
-            }
-          />
+                  <p>
+                    <Label
+                      labelText="Otrzymam:"
+                    />
+                    <strong> {calculateToPlnResult()} </strong>
+                    PLN
+                  </p>
+                </>
+              }
+            />
 
-          <Fieldset
-            title="Wymiana na walutę obcą"
-            body={
-              <>
-                <Container
-                  body={
-                    <>
-                      <Label
-                        forContent="amountFromPln"
-                        labelText="Posiadam:"
-                      />
-                      <Input
-                        value={amountFromPln}
-                        onChange={handleFromPlnInput}
-                        id="amountFromPln"
-                        onKeyDown={handleKeyDown}
-                      />
-                      PLN
-                    </>
-                  }
-                />
-                <p>
-                  <Label
-                    forContent="fromPln"
-                    labelText="Otrzymam:"
+            <Fieldset
+              title="Wymiana na walutę obcą"
+              body={
+                <>
+                  <Container
+                    body={
+                      <>
+                        <Label
+                          forContent="amountFromPln"
+                          labelText="Posiadam:"
+                        />
+                        <Input
+                          value={amountFromPln}
+                          onChange={handleFromPlnInput}
+                          id="amountFromPln"
+                          onKeyDown={handleKeyDown}
+                        />
+                        PLN
+                      </>
+                    }
                   />
-                  <strong>{calculateFromPlnResult()} </strong>
-                  <Select
-                    name="fromPln"
-                    id="fromPln"
-                    defaultValue={currencies[0].code}
-                    onChange={handleFromPlnSelect}
-                    currencies={currencies}
-                    renderOptionContent={(currency) => currency.code}
-                  />
-                </p>
-              </>
-            }
-          />
-        </>
+                  <p>
+                    <Label
+                      forContent="fromPln"
+                      labelText="Otrzymam:"
+                    />
+                    <strong>{calculateFromPlnResult()} </strong>
+                    <Select
+                      name="fromPln"
+                      id="fromPln"
+                      defaultValue={currencies[0].code}
+                      onChange={handleFromPlnSelect}
+                      currencies={currencies}
+                      renderOptionContent={(currency) => currency.code}
+                    />
+                  </p>
+                </>
+              }
+            />
+          </>
         ) : (
           <div>Ładowanie danych...</div>
         )
---
+++ src/Currencies/styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Update = styled.p`
+    text-align: center;
+    font-family: Inconsolata;
+`;
